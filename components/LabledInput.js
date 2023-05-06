0f1cab605e0014be97e3508d46b7daf47a2aaf0b
Quinten Favorite <quintenfavorite97@gmail.com>
2023-05-06 02:55:38 -0400

    added example of css outside of js file

--- components/LabledInput.js
+++ components/LabledInput.js
@@ -1,29 +1,16 @@
+import "./labledInput.css";
+
 const LabeledInput = (labelText, inputType, isRequired) => {
   const inputId = `${labelText.toLowerCase().replace(/\s/g, "-")}-input`;
   const labelFor = inputId;
 
-  const style = {
-    label: `
-      font-weight: bold;
-      margin-bottom: 0.5rem;
-    `,
-    input: `
-      padding: 0.5rem;
-      font-size: 1rem;
-      border-radius: 0.25rem;
-      border: 1px solid #ccc;
-    `,
-  };
   return `
-  <label for="${labelFor}" 
-    style="${style.label}"
-    >
+  <label for="${labelFor}">
     ${labelText}
   </label>
   <input 
     type="${inputType}" 
     id="${inputId} 
-    style="${style.input}" 
     name="${inputId}"
     ${isRequired ? " required" : ""}
     >`;
